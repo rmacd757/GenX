cd8409b77ec2fb3ea4f6fee7b52979e8043548b1
RuaridhMacd <rmacd@mit.edu>
2022-12-11 21:12:43 -0500

    Initial runs to test baseline and case manipulation

--- TEGS_runs/run_files/Run_emissions_and_baseline.jl
+++ TEGS_runs/run_files/Run_emissions_and_baseline.jl
@@ -1,4 +1,5 @@
 using GenX
+using CSV
 using DataFrames
 
 ############################################
@@ -24,6 +25,21 @@ function time_domain_reduced_files_exist(tdrpath)
     return (tdr_load && tdr_genvar && tdr_fuels)
 end
 
+function build_solve_write(outputs_path, mysetup, myinputs, OPTIMIZER)
+    println("Generating the Optimization Model")
+    EP = generate_model(mysetup, myinputs, OPTIMIZER)
+
+    println("Solving Model")
+    EP, solve_time = solve_model(EP, mysetup)
+    myinputs["solve_time"] = solve_time # Store the model solve time in myinputs
+
+    # Run MGA if the MGA flag is set to 1 else only save the least cost solution
+    println("Writing Output")
+    elapsed_time = @elapsed write_outputs(EP, outputs_path, mysetup, myinputs)
+    println("Time elapsed for writing is")
+    println(elapsed_time)
+    return EP
+end
 ############################################
 # Case Definitions 
 # All cases intended to be run from the run-file directory
@@ -35,53 +51,80 @@ location_dir = Dict{String, String}(
     # "texas" => joinpath(root_dir, "data", "texas"),
 )
 
-scenarios = ["emission_baseline"]
+logging_notes = Array{String, 1}()
 
 for (loc_name, loc_path) in location_dir
-    for scenario in scenarios
-        case = loc_path
-        genx_settings = get_settings_path(case, "genx_settings.yml") #Settings YAML file path
-        mysetup = configure_settings(genx_settings) # mysetup dictionary stores settings and GenX-specific parameters
+    case = loc_path
+    genx_settings = get_settings_path(case, "genx_settings.yml") #Settings YAML file path
+    mysetup = configure_settings(genx_settings) # mysetup dictionary stores settings and GenX-specific parameters
 
-        inputs_path = case
-        settings_path = get_settings_path(case)
+    inputs_path = case
+    settings_path = get_settings_path(case)
 
-        outputs_path = joinpath(root_dir, "outputs", loc_name)
-        outputs_path = joinpath(outputs_path, scenario)
+    outputs_path_root = joinpath(root_dir, "outputs", loc_name, "emissions_and_baseline")
+    if !isdir(outputs_path_root)
+        mkpath(outputs_path_root)
+    end
 
-        ### Cluster time series inputs if necessary and if specified by the user
-        TDRpath = joinpath(case, mysetup["TimeDomainReductionFolder"])
+    ### Cluster time series inputs if necessary and if specified by the user
+    TDRpath = joinpath(case, mysetup["TimeDomainReductionFolder"])
 
-        if mysetup["TimeDomainReduction"] == 1
-            if !time_domain_reduced_files_exist(TDRpath)
-                println("Clustering Time Series Data (Grouped)...")
-                cluster_inputs(inputs_path, settings_path, mysetup)
-            else
-                println("Time Series Data Already Clustered.")
-            end
+    if mysetup["TimeDomainReduction"] == 1
+        if !time_domain_reduced_files_exist(TDRpath)
+            println("Clustering Time Series Data (Grouped)...")
+            cluster_inputs(inputs_path, settings_path, mysetup)
+        else
+            println("Time Series Data Already Clustered.")
         end
+    end
 
-        ### Configure solver
-        println("Configuring Solver")
-        OPTIMIZER = configure_solver(mysetup["Solver"], settings_path)
+    ### Configure solver
+    println("Configuring Solver")
+    OPTIMIZER = configure_solver(mysetup["Solver"], settings_path)
 
-        #### Running a case
+    #### Running a case
 
-        ### Load inputs
-        println("Loading Inputs")
-        myinputs = load_inputs(mysetup, case)
+    ### Load inputs
+    println("Loading Inputs")
+    myinputs = load_inputs(mysetup, case)
 
-        println("Generating the Optimization Model")
-        EP = generate_model(mysetup, myinputs, OPTIMIZER)
+    # Calculate and save baseline emissions
+    outputs_path = joinpath(outputs_path_root, "baseline")
+    EP = build_solve_write(outputs_path, mysetup, myinputs, OPTIMIZER)
 
-        println("Solving Model")
-        EP, solve_time = solve_model(EP, mysetup)
-        myinputs["solve_time"] = solve_time # Store the model solve time in myinputs
+    println
+    push!(logging_notes, "Baseline results written to $outputs_path\n")
 
-        # Run MGA if the MGA flag is set to 1 else only save the least cost solution
-        println("Writing Output")
-        elapsed_time = @elapsed write_outputs(EP, outputs_path, mysetup, myinputs)
-        println("Time elapsed for writing is")
-        println(elapsed_time)
+    # Find the emissions level
+    # Load from the written results to make this usable with old results
+    # DataFrame is overkill here, but more flexible
+    emissions_df = DataFrame(CSV.File(joinpath(outputs_path, "emissions.csv")))
+    baseline_emiss = emissions_df.Total[1]
+
+    push!(logging_notes, "Baseline emissions were $baseline_emiss\n")
+
+    emissions_levels = Dict{String, Float64}(
+        "0.20"=>0.2,
+        "0.10"=>0.1, 
+        "0.05"=>0.05,
+        "0.01"=>0.01)
+    scale_factor = mysetup["ParameterScale"] == 1 ? ModelScalingFactor : 1
+
+    
+    mysetup["CO2Cap"] = 1
+    myinputs = load_inputs(mysetup, case) # Reload CO2_cap settings
+    push!(logging_notes, "Emissions cap activated and inputs reloaded\n")
+
+    for (emiss_name, emiss_level) in emissions_levels
+        emiss_target = emiss_level * baseline_emiss
+        push!(logging_notes, "Emissions target is $emiss_target\n")
+
+        myinputs["dfMaxCO2"] = emiss_target * 1e3 / scale_factor
+
+        # Calculate and save baseline emissions
+        outputs_path = joinpath(outputs_path_root, emiss_name)
+        EP = build_solve_write(outputs_path, mysetup, myinputs, OPTIMIZER)
+
+        push!(logging_notes, "$emiss_target results written to $outputs_path\n")
     end
 end
