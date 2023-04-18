41751d5d1060ea5fbbc7c5307d6cdb7c14bae670
nirmalb <nirmalb@mit.edu>
2023-04-18 10:47:55 -0400

    Small Output update
    1) made all fusion output files be in the fusion folder in results

--- src/write_outputs/write_fusion.jl
+++ src/write_outputs/write_fusion.jl
@@ -63,36 +63,64 @@ function get_fusion_data_vector(EP::JuMP.Model, model_key::Symbol, R_ID::Int64, 
     return vec(value.(EP[model_key][[R_ID], mintime:maxtime]))
 end
 
+function get_fusion_fleet_data(EP::JuMP.Model, model_key::Symbol, R_ID::Int64)
+    return value(EP[model_key][R_ID])
+end
+
 function write_fusion_var(path::AbstractString, inputs::Dict, setup::Dict, EP::Model)
     dfFusion = inputs["dfFusion"]
     FUSION = inputs["FUSION"]
     dfGen = inputs["dfGen"]
 
-
-    numunitf = zeros(size(inputs["dfGen"]))
-    tritcap = zeros(size(inputs["dfGen"]))
-    deucap = zeros(size(inputs["dfGen"]))
-    fthermstorcap = zeros(size(inputs["dfGen"]))
-    fthermdiscap = zeros(size(inputs["dfGen"]))
-    
-    for i in FUSION
-        numunitf[i] = value(EP[:num_units][i])
-        tritcap[i] = value(EP[:vTritCap][i])
-        deucap[i] = value(EP[:vDeuCap][i])            
-        # fthermstorcap[i] = value(EP[:vThermStorCap][i])
-        # fthermdiscap[i] = value(EP[:vThermDisCap][i])
+    # Make ~/fusion directory if it doesn't exist
+    fusion_results_path = joinpath(path, "fusion")
+    if !isdir(fusion_results_path)
+        mkpath(fusion_results_path)
     end
 
-    f_var = DataFrame(
-        Fusion_tech = inputs["FUSION"],
-        Num_Units = numunitf[FUSION],
-        Trit_Cap = tritcap[FUSION],
-        Deu_Cap = deucap[FUSION],
-        # Therm_Stor_Cap = fthermstorcap[FUSION],
-        # Therm_Dis_Cap = fthermdiscap[FUSION]
-    )
+    for y in FUSION
+        file_name = string("fusion_var_", y, ".csv")
 
-    CSV.write(joinpath(path, "fusion_var.csv"), f_var)
+        fusion_variables = OrderedDict{String, Any}(
+            "Num_Units" => :num_units,
+            "Tritium Cap" => :vTritCap,
+            "Deuterium Cap" => :vDeuCap,
+        )
+        for (name, model_key) in fusion_variables
+            if haskey(EP, model_key)
+                fusion_variables[name] = get_fusion_fleet_data(EP, model_key, y)
+            else
+                fusion_variables[name] = zeros(1)
+            end
+        end
+        fusion_var_df = DataFrame(fusion_variables)    
+        CSV.write(joinpath(fusion_results_path, file_name), fusion_var_df)
+    end
+
+    # numunitf = zeros(size(inputs["dfGen"]))
+    # tritcap = zeros(size(inputs["dfGen"]))
+    # deucap = zeros(size(inputs["dfGen"]))
+    # fthermstorcap = zeros(size(inputs["dfGen"]))
+    # fthermdiscap = zeros(size(inputs["dfGen"]))
+    
+    # for i in FUSION
+    #     numunitf[i] = value(EP[:num_units][i])
+    #     tritcap[i] = value(EP[:vTritCap][i])
+    #     deucap[i] = value(EP[:vDeuCap][i])            
+    #     # fthermstorcap[i] = value(EP[:vThermStorCap][i])
+    #     # fthermdiscap[i] = value(EP[:vThermDisCap][i])
+    # end
+
+    # f_var = DataFrame(
+    #     Fusion_tech = inputs["FUSION"],
+    #     Num_Units = numunitf[FUSION],
+    #     Trit_Cap = tritcap[FUSION],
+    #     Deu_Cap = deucap[FUSION],
+    #     # Therm_Stor_Cap = fthermstorcap[FUSION],
+    #     # Therm_Dis_Cap = fthermdiscap[FUSION]
+    # )
+
+    # CSV.write(joinpath(path, "fusion_var.csv"), f_var)
 end
 
 
