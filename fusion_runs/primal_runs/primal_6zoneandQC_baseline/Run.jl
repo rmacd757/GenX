f44f7a3f3f93bd0884b779effeda32807c3236e5
Ruaridh Macdonald <rmacd@mit.edu>
2023-04-23 09:05:29 -0400

    Hydro baseline updates

--- fusion_runs/primal_runs/primal_6zoneandQC_baseline/Run.jl
+++ fusion_runs/primal_runs/primal_6zoneandQC_baseline/Run.jl
@@ -1,42 +1,23 @@
 using GenX
 using JuMP
-
 using OrderedCollections
 using DataFrames
 using CSV
 
-case = joinpath(pwd(),"fusion_runs","data","primal_6zoneandQC_baseline")
+## Load helper functions
+include(joinpath(pwd(),"fusion_runs","run_helpers.jl"))
 
+## Define input and output paths
+inputs_path = joinpath(pwd(),"fusion_runs","data","primal_6zoneandQC_baseline")
 outputs_path = dirname(@__FILE__)
 
-function get_settings_path(case::AbstractString)
-    return joinpath(case, "Settings")
-end
-
-function get_settings_path(case::AbstractString, filename::AbstractString)
-    return joinpath(get_settings_path(case), filename)
-end
-
-function get_default_output_folder(case::AbstractString)
-    return joinpath(case, "Results")
-end
-
-genx_settings = get_settings_path(case, "genx_settings.yml") #Settings YAML file path
+## Load settings
+genx_settings = get_settings_path(inputs_path, "genx_settings.yml") #Settings YAML file path
 mysetup = configure_settings(genx_settings) # mysetup dictionary stores settings and GenX-specific parameters
+settings_path = get_settings_path(inputs_path)
 
-inputs_path = case
-settings_path = get_settings_path(case)
-
-### Cluster time series inputs if necessary and if specified by the user
-TDRpath = joinpath(case, mysetup["TimeDomainReductionFolder"])
-
-function time_domain_reduced_files_exist(tdrpath)
-    tdr_load = isfile(joinpath(tdrpath,"Load_data.csv"))
-    tdr_genvar = isfile(joinpath(tdrpath,"Generators_variability.csv"))
-    tdr_fuels = isfile(joinpath(tdrpath,"Fuels_data.csv"))
-    return (tdr_load && tdr_genvar && tdr_fuels)
-end
-
+## Cluster time series inputs if necessary and if specified by the user
+TDRpath = joinpath(inputs_path, mysetup["TimeDomainReductionFolder"])
 if mysetup["TimeDomainReduction"] == 1
     if !time_domain_reduced_files_exist(TDRpath)
         println("Clustering Time Series Data (Grouped)...")
@@ -46,28 +27,38 @@ if mysetup["TimeDomainReduction"] == 1
     end
 end
 
-### Configure solver
+## Configure solver
 println("Configuring Solver")
 OPTIMIZER = configure_solver(mysetup["Solver"], settings_path)
 set_optimizer_attribute(OPTIMIZER, "BarHomogeneous", 1)
 
 #### Running a case
 
-### Load inputs
+## Load inputs
 println("Loading Inputs")
-myinputs = load_inputs(mysetup, case)
+myinputs = load_inputs(mysetup, inputs_path)
 
-mysetup["CO2Cap"] = 1
-scale_factor = mysetup["ParameterScale"] == 1 ? ModelScalingFactor : 1
-
+## Generate model
 println("Generating the Optimization Model")
 EP = generate_model(mysetup, myinputs, OPTIMIZER)
 
+#### Add any additional constraints
+HYDRO_RES = myinputs["HYDRO_RES"]
+
+## Hydro storage <= 0.55 * Existing Capacity at start of May 1st
+@constraint(EP, EP[:vS_HYDRO][HYDRO_RES, 2879] .== 0.55 .* (dfGen[HYDRO_RES,:Existing_Cap_MWh]))
+
+## Hydro storage == 0.70 * Existing Capacity at the start of the year
+@constraint(EP, EP[:vS_HYDRO][HYDRO_RES, 1] .== 0.70 .* (dfGen[HYDRO_RES,:Existing_Cap_MWh]))
+
+## Solve model
 println("Solving Model")
 EP, solve_time = solve_model(EP, mysetup)
 myinputs["solve_time"] = solve_time # Store the model solve time in myinputs
 
-# Run MGA if the MGA flag is set to 1 else only save the least cost solution
+## Run MGA if the MGA flag is set to 1 else only save the least cost solution
 println("Writing Output")
 outputs_path = get_default_output_folder(outputs_path)
+
+## Write outputs
 write_outputs(EP, outputs_path, mysetup, myinputs)
