2742ff4357ea35ede945273830986b22967c65d2
Ruaridh Macdonald <rmacd@mit.edu>
2023-08-03 11:22:55 -0400

    Update Run.jl

--- fusion_paper/paper_runs/dual_runs/2z_20sc_dual_basecase_SC/Run.jl
+++ fusion_paper/paper_runs/dual_runs/2z_20sc_dual_basecase_SC/Run.jl
@@ -82,103 +82,111 @@ for i in task_id+1:num_tasks:length(all_cases)
     emiss_lim = all_cases[i][1]
     fusion_cap = all_cases[i][2]
 
-    myinputs["dfMaxCO2"][2] = emiss_lim * 1e3 / scale_factor
-    outputs_path = joinpath(results_path, "Dual_" * string(fusion_cap) * "mw_EmissLevel_" * string(emiss_lim))
+    println("Emiss Limit: $emiss_lim, Fusion Cap: $fusion_cap")
 
-    # Find all the fusion resources in the model
-    # and set their investment and fixed O&M costs to zero
-    dfGen = myinputs["dfGen"]
-    fusion_rid = findall(x -> startswith(x, "fusion"), dfGen[!,:Resource])
-    for y in fusion_rid
-        dfGen[y,:Inv_Cost_per_MWyr] = 0.0
-        dfGen[y,:Fixed_OM_Cost_per_MWyr] = 0.0
-    end
+    try
 
-    # This check will cause the case to be skipped if the results already exist
-    if isfile(joinpath(outputs_path, "costs.csv"))
-        println("Skipping Case for emiss limit = " * string(emiss_lim) * " because it already exists.")
-        continue
-    end
+        myinputs["dfMaxCO2"][2] = emiss_lim * 1e3 / scale_factor
+        outputs_path = joinpath(results_path, "Dual_" * string(fusion_cap) * "mw_EmissLevel_" * string(emiss_lim))
 
-    mkpath(dirname(outputs_path))
-    
-    ## Generate model
-    println("Generating the Optimization Model")
-    EP = generate_model(mysetup, myinputs, OPTIMIZER)
+        # Find all the fusion resources in the model
+        # and set their investment and fixed O&M costs to zero
+        dfGen = myinputs["dfGen"]
+        fusion_rid = findall(x -> startswith(x, "fusion"), dfGen[!,:Resource])
+        for y in fusion_rid
+            dfGen[y,:Inv_Cost_per_MWyr] = 0.0
+            dfGen[y,:Fixed_OM_Cost_per_MWyr] = 0.0
+        end
 
-    ########################
-    #### Add any additional constraints
-    HYDRO_RES = myinputs["HYDRO_RES"]
+        # This check will cause the case to be skipped if the results already exist
+        if isfile(joinpath(outputs_path, "costs.csv"))
+            println("Skipping Case for emiss limit = " * string(emiss_lim) * " because it already exists.")
+            continue
+        end
 
-    # Empty arrays for indexing
-    yr_start = Int[]
-    yr_mid = Int[]
-
-    # 20 year indexing
-    for i in 1:20
-        # Calculate the value for the beginning of the year
-        start_year = (i-1) * 8760 + 1
-        push!(yr_start, start_year)
-
-        # Calculate the value for the middle of the year
-        mid_year = (i-1) * 8760 + 2879
-        push!(yr_mid, mid_year)
-    end
-
-    ## Hydro storage <= 0.55 * Existing Capacity at start of May 1st 
-    @constraint(EP, cHydroSpring[y in HYDRO_RES, i in yr_mid], EP[:vS_HYDRO][y, i] .<= 0.55 .* EP[:eTotalCap][y] .* dfGen[y,:Hydro_Energy_to_Power_Ratio])
-
-    ## Hydro storage == 0.70 * Existing Capacity at the start of the year
-    @constraint(EP, cHydroJan[y in HYDRO_RES, i in yr_start], EP[:vS_HYDRO][y, i]  .== 0.70 .* EP[:eTotalCap][y] .* dfGen[y,:Hydro_Energy_to_Power_Ratio])
+        mkpath(dirname(outputs_path))
         
-    ## Maine -> Quebec transmission limited to 2170MWe.
-    # The line is defined as Quebec -> Maine in Network.csv, so these flows will be negative
-    # Make sure to correc the line index if the order is changed in Network.csv
-    @constraint(EP, cMaine2Quebec[t=1:myinputs["T"]], EP[:vFLOW][2, t] >= -170.0)
+        ## Generate model
+        println("Generating the Optimization Model")
+        EP = generate_model(mysetup, myinputs, OPTIMIZER)
 
-    # ## Solar <= 22GWe
-    # solar_rid = findall(x -> startswith(x, "solar"), dfGen[!,:Resource])
-    # @constraint(EP, cSolarCap, sum(EP[:eTotalCap][y] for y in solar_rid) <= 22e3)
+        ########################
+        #### Add any additional constraints
+        HYDRO_RES = myinputs["HYDRO_RES"]
 
-    # ## Commercial solar <= 15GWe
-    # comm_solar_rid = findall(x -> startswith(x, "comm_solar"), dfGen[!,:Resource])
-    # @constraint(EP, cCommSolarCap, sum(EP[:eTotalCap][y] for y in comm_solar_rid) <= 15e3)
+        # Empty arrays for indexing
+        yr_start = Int[]
+        yr_mid = Int[]
 
-    # ## Residential solar <= 10GWe
-    # res_solar_rid = findall(x -> startswith(x, "res_solar"), dfGen[!,:Resource])
-    # @constraint(EP, cResSolarCap, sum(EP[:eTotalCap][y] for y in res_solar_rid) <= 10e3)
+        # 20 year indexing
+        for i in 1:20
+            # Calculate the value for the beginning of the year
+            start_year = (i-1) * 8760 + 1
+            push!(yr_start, start_year)
 
-    # ## Onshore wind <= 10GWe
-    # onshore_rid = findall(x -> startswith(x, "onshore"), dfGen[!,:Resource])
-    # @constraint(EP, cOnshoreCap, sum(EP[:eTotalCap][y] for y in onshore_rid) <= 10e3)
+            # Calculate the value for the middle of the year
+            mid_year = (i-1) * 8760 + 2879
+            push!(yr_mid, mid_year)
+        end
 
-    # ## Fixed offshore wind <= 37.5We
-    # fixed_offshore_rid = findall(x -> startswith(x, "fixed_offshore"), dfGen[!,:Resource])
-    # @constraint(EP, cFixedOffshoreCap, sum(EP[:eTotalCap][y] for y in fixed_offshore_rid) <= 37.5e3)
+        ## Hydro storage <= 0.55 * Existing Capacity at start of May 1st 
+        @constraint(EP, cHydroSpring[y in HYDRO_RES, i in yr_mid], EP[:vS_HYDRO][y, i] .<= 0.55 .* EP[:eTotalCap][y] .* dfGen[y,:Hydro_Energy_to_Power_Ratio])
 
-    # ## Floating offshore wind <= 275GWe
-    # floating_offshore_rid = findall(x -> startswith(x, "float_offshore"), dfGen[!,:Resource])
-    # @constraint(EP, cFloatOffshoreCap, sum(EP[:eTotalCap][y] for y in floating_offshore_rid) <= 275e3)
+        ## Hydro storage == 0.70 * Existing Capacity at the start of the year
+        @constraint(EP, cHydroJan[y in HYDRO_RES, i in yr_start], EP[:vS_HYDRO][y, i]  .== 0.70 .* EP[:eTotalCap][y] .* dfGen[y,:Hydro_Energy_to_Power_Ratio])
+            
+        ## Maine -> Quebec transmission limited to 2170MWe.
+        # The line is defined as Quebec -> Maine in Network.csv, so these flows will be negative
+        # Make sure to correc the line index if the order is changed in Network.csv
+        @constraint(EP, cMaine2Quebec[t=1:myinputs["T"]], EP[:vFLOW][2, t] >= -170.0)
 
-    ## Fusion <= fusion_cap
-    @constraint(EP, cFusionCap, sum(EP[:eTotalCap][y] for y in fusion_rid) <= fusion_cap)
-    ## Set fusion investment and fixed O&M costs to zero
-    
-    ########################
+        # ## Solar <= 22GWe
+        # solar_rid = findall(x -> startswith(x, "solar"), dfGen[!,:Resource])
+        # @constraint(EP, cSolarCap, sum(EP[:eTotalCap][y] for y in solar_rid) <= 22e3)
 
-    ## Solve model
-    println("Solving Model")
-    EP, solve_time = solve_model(EP, mysetup)
-    myinputs["solve_time"] = solve_time # Store the model solve time in myinputs
+        # ## Commercial solar <= 15GWe
+        # comm_solar_rid = findall(x -> startswith(x, "comm_solar"), dfGen[!,:Resource])
+        # @constraint(EP, cCommSolarCap, sum(EP[:eTotalCap][y] for y in comm_solar_rid) <= 15e3)
 
-    ## Run MGA if the MGA flag is set to 1 else only save the least cost solution
-    println("Writing Output")
-    # outputs_path = get_default_output_folder(outputs_path)
+        # ## Residential solar <= 10GWe
+        # res_solar_rid = findall(x -> startswith(x, "res_solar"), dfGen[!,:Resource])
+        # @constraint(EP, cResSolarCap, sum(EP[:eTotalCap][y] for y in res_solar_rid) <= 10e3)
 
-    ## Write outputs
-    write_outputs(EP, outputs_path, mysetup, myinputs)
+        # ## Onshore wind <= 10GWe
+        # onshore_rid = findall(x -> startswith(x, "onshore"), dfGen[!,:Resource])
+        # @constraint(EP, cOnshoreCap, sum(EP[:eTotalCap][y] for y in onshore_rid) <= 10e3)
 
-    result_summ = DataFrame(Cost=objective_value(EP), Dual=dual(EP[:cFusionCap]))
-    CSV.write(joinpath(outputs_path, "fpp_results.csv"), result_summ)
+        # ## Fixed offshore wind <= 37.5We
+        # fixed_offshore_rid = findall(x -> startswith(x, "fixed_offshore"), dfGen[!,:Resource])
+        # @constraint(EP, cFixedOffshoreCap, sum(EP[:eTotalCap][y] for y in fixed_offshore_rid) <= 37.5e3)
+
+        # ## Floating offshore wind <= 275GWe
+        # floating_offshore_rid = findall(x -> startswith(x, "float_offshore"), dfGen[!,:Resource])
+        # @constraint(EP, cFloatOffshoreCap, sum(EP[:eTotalCap][y] for y in floating_offshore_rid) <= 275e3)
+
+        ## Fusion <= fusion_cap
+        @constraint(EP, cFusionCap, sum(EP[:eTotalCap][y] for y in fusion_rid) <= fusion_cap)
+        ## Set fusion investment and fixed O&M costs to zero
+        
+        ########################
+
+        ## Solve model
+        println("Solving Model")
+        EP, solve_time = solve_model(EP, mysetup)
+        myinputs["solve_time"] = solve_time # Store the model solve time in myinputs
+
+        ## Run MGA if the MGA flag is set to 1 else only save the least cost solution
+        println("Writing Output")
+        # outputs_path = get_default_output_folder(outputs_path)
+
+        ## Write outputs
+        write_outputs(EP, outputs_path, mysetup, myinputs)
+
+        result_summ = DataFrame(Cost=objective_value(EP), Dual=dual(EP[:cFusionCap]))
+        CSV.write(joinpath(outputs_path, "fpp_results.csv"), result_summ)
+
+    catch e
+        println("Run failed using Emiss Limit: $emiss_lim, Fusion Cap: $fusion_cap")
+    end
 end
 
