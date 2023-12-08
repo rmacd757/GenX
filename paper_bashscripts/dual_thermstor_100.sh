ba076a526fe1fa9e93cd02d0b0d678d4c0e576f2
Ruaridh Macdonald <RuaridhMacd>
2023-12-08 11:44:37 -0500

    Moving old bashscripts - pt1

---
+++ paper_bashscripts/dual_thermstor_100.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+source /etc/profile
+module load julia/1.8.5
+module load gurobi/gurobi-1000
+
+echo "My SLURM_ARRAY_TASK_ID: " $LLSUB_RANK
+echo "Number of Tasks: " $LLSUB_SIZE
+
+julia --project=. fusion_paper/paper_runs/dual_runs/2z_20sc_dual_100percent_thermStor_SC/Run.jl $LLSUB_RANK $LLSUB_SIZE 16
+
