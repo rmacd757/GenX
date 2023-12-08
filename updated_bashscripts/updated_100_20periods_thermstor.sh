0fa0384f05aab2548d8860d656511d31ca4d560d
Ruaridh Macdonald <RuaridhMacd>
2023-12-08 11:45:47 -0500

    Move scripts for updated inputs

---
+++ updated_bashscripts/updated_100_20periods_thermstor.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+source /etc/profile
+module load julia/1.8.5
+module load gurobi/gurobi-1000
+
+echo "My SLURM_ARRAY_TASK_ID: " $LLSUB_RANK
+echo "Number of Tasks: " $LLSUB_SIZE
+
+julia --project=. fusion_paper/paper_runs/dual_runs/updated_inputs/update_2z_20sc_20periods_dual_100percent_SC_thermStorVarTurbine/Run.jl $LLSUB_RANK $LLSUB_SIZE 16
+
