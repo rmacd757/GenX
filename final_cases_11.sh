0991b4994cbbf151f233965ebe9a03bb54c9531e
Ruaridh Macdonald <rmacd@mit.edu>
2024-02-14 08:49:52 -0500

    Run files for new cases

--- final_cases_11.sh
+++ final_cases_11.sh
@@ -7,6 +7,7 @@ module load gurobi/gurobi-1000
 echo "My SLURM_ARRAY_TASK_ID: " $LLSUB_RANK
 echo "Number of Tasks: " $LLSUB_SIZE
 
-julia --project=. fusion_paper/paper_runs/dual_runs/final_report_runs/final_thermstor_10_noLeak/Run.jl $LLSUB_RANK $LLSUB_SIZE 16
-julia --project=. fusion_paper/paper_runs/dual_runs/final_report_runs/final_thermstor_10_noLeak_cheapTurb/Run.jl $LLSUB_RANK $LLSUB_SIZE 16
+# julia --project=. fusion_paper/paper_runs/dual_runs/final_report_runs/final_thermstor_10_noLeak/Run.jl $LLSUB_RANK $LLSUB_SIZE 16
+# julia --project=. fusion_paper/paper_runs/dual_runs/final_report_runs/final_thermstor_10_noLeak_cheapTurb/Run.jl $LLSUB_RANK $LLSUB_SIZE 16
+julia --project=. fusion_paper/paper_runs/dual_runs/final_report_runs/final_thermstor_10_noLeak_cheapTurb500/Run.jl $LLSUB_RANK $LLSUB_SIZE 16
 
